6e51b5c8a0c925aff6330a7b970c73139ab5f78f
Brendan William Demilt <bdemilt@umd.edu>
2020-03-30 19:47:17 -0400

    Initial commit

---
+++ OurQueue/OurQueue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OurQueue
+//
+//  Created by Brendan DeMilt on 3/30/20.
+//  Copyright © 2020 Brendan DeMilt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
