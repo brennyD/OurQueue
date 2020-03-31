fffebca34e9212193cd0f2145608dc6c7d09fed2
Brendan William Demilt <bdemilt@umd.edu>
2020-03-30 21:07:54 -0400

    idk what i did but it's working?

---
+++ OurQueue/OurQueue/OurQueue-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  OurQueue-Bridging-Header.h
+//  OurQueue
+//
+//  Created by Brendan DeMilt on 3/30/20.
+//  Copyright © 2020 Brendan DeMilt. All rights reserved.
+//
+
+#ifndef OurQueue_Bridging_Header_h
+#define OurQueue_Bridging_Header_h
+
+
+#endif
+
+#import <SpotifyiOS/SpotifyiOS.h>
